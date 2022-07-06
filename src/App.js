c71e24219aade3c9fb32543c7b8d3579cfb25524
rogallardo <rodrigogastongallardo@gmail.com>
2022-07-05 21:03:29 -0300

    desafio ItemCount

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
-import ItemDetailContainer  from './components/itemDetailContainer/ItemDetailContainer';
+import ItemDetailContainer  from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
